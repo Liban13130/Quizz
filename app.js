b30d1fe218e1dfcad675c3117a734e3541cd32b9
liban13130 <nabil-13130@hotmail.fr>
2023-04-28 02:24:26 +0200

    work

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ const responses = ["c", "a", "b", "a", "c"];
 const emojis = ["✔️", "✨", "👀", "😭", "👎"];
 
 const form = document.querySelector('.quiz-form')
+form.addEventListener("submit", handleSubmit)
+
+function handleSubmit(e){
+    e.preventDefault()
+
+    const results = [];
+
+    const radioButtons = document.querySelectorAll("input[type='radio']:checked")
+
+    console.log(radioButtons);
+}
